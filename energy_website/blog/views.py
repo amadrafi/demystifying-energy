c74b016a88f6634beeb29c9d16a3d59b674cd29a
amadrafi <ahmadrafi0814@gmail.com>
2020-04-08 15:25:08 +0700

    Adding a database for the website

--- energy_website/blog/views.py
+++ energy_website/blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from .models import Article
 # Create your views here.
 
 posts = [
     {
         'author': 'Irfan',
         'title': 'Blog post 1',
-        'content': 'First post content',
+        'content': 'Post Content',
         'date_posted': 'April 7, 2020'
     },
     {
@@ -15,12 +16,15 @@ posts = [
         'date_posted': 'April 8, 2020'
     }
 ]
-
 def home(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/home.html', context)
+    articles = Article.objects.all()
+    return render(request, 'blog/home.html', {'articles':articles})
+
+# def home(request):
+#     context = {
+#         'posts': posts
+#     }
+#     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html',{'title': 'About'})
