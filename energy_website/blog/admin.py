f050632f66ddf47d030ce18aa996426cd273aadb
gagaga <gagagag@gmail.com>
2020-04-09 18:05:05 +0800

    added model to admin site

--- energy_website/blog/admin.py
+++ energy_website/blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Article, ArticleAdmin)
--- energy_website/blog/models.py
+++ energy_website/blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
@@ -8,6 +13,11 @@ class Article(models.Model):
     summary = models.CharField(max_length=100)
     datePosted = models.DateTimeField(auto_now_add=True)
     author = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
