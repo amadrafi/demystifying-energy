eda2c0938a8b99f4fd8bad18ae84d048888ffb20
WReverse <56021409+WReverse@users.noreply.github.com>
2020-04-10 14:27:19 +0700

    Added categories, allowed only posts to be seen.

--- energy_website/blog/models.py
+++ energy_website/blog/models.py
@@ -5,10 +5,23 @@ STATUS = (
     (0,"Draft"),
     (1,"Publish")
 )
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category' 
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     content = models.TextField()
     summary = models.CharField(max_length=100)
     datePosted = models.DateTimeField(auto_now_add=True)
