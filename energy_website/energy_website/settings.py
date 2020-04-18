6a19eecf5913265b0c4b3b845a11c4c7ab6cfefb
WReverse <56021409+WReverse@users.noreply.github.com>
2020-04-18 15:56:06 +0700

    Changed static files directory

--- energy_website/energy_website/settings.py
+++ energy_website/energy_website/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -116,14 +115,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
 CKEDITOR_CONFIGS = { 'default': 
                          { 'toolbar': 'Custom', 'height': 500, 'toolbar_Custom': 
                              [ 
