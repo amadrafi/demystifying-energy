fe8219d1219efc5637727b31df93c66e702f4d42
WReverse <56021409+WReverse@users.noreply.github.com>
2020-07-29 20:34:12 +0700

    fixed staticfiles_storage

--- energy_website/energy_website/settings.py
+++ energy_website/energy_website/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
 STATIC_URL = '/static/'
 
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CKEDITOR_CONFIGS = { 'default': 
                          { 'toolbar': 'Custom', 'height': 500, 'toolbar_Custom': 
